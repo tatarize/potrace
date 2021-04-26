0651081b6d8408ef2344a244876344244a3c30f4
Tatarize <tatarize@gmail.com>
2021-04-25 21:29:09 -0700

    Prep for Time Optimization

--- potrace/main.py
+++ potrace/main.py
@@ -118,7 +118,22 @@ parser.add_argument(
     help="Dither rather than threshold to 1-bit.",
 )
 
+def timeit(method):
+    def timed(*args, **kw):
+        import time
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % \
+                  (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
 
+@timeit
 def run():
     argv = sys.argv[1:]
     args = parser.parse_args(argv)
